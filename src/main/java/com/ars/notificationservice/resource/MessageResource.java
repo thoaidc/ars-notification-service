1f9c4260e833f650cded325dfd2c3fa5648db08e
Đàm Công Thoại <dcthoai1023@gmail.com>
2025-12-25 15:46:06 +0700

    fix

--- src/main/java/com/ars/notificationservice/resource/MessageResource.java
+++ src/main/java/com/ars/notificationservice/resource/MessageResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/p/v1/messages")
+@RequestMapping("/api/internal/v1/messages")
 public class MessageResource {
     private final WebSocketClient webSocketClient;
 
